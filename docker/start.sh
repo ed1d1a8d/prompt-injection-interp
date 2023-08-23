a8f56ca1401b01c4a0e7944f8f054c2d9fde84b0
Tony Wang <twang6@mit.edu>
2023-08-23 16:21:28 -0700

    Update container

--- docker/start.sh
+++ docker/start.sh
@@ -3,12 +3,13 @@
 
 set -e  # Exit the script if any statement returns a non-true return value
 
+# Setup ssh
 if [[ $PUBLIC_KEY ]]; then
     echo "Setting up SSH..."
     mkdir -p ~/.ssh
     echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
     chmod 700 -R ~/.ssh
-    service ssh start
+    sudo service ssh start
 fi
 
 sleep infinity
